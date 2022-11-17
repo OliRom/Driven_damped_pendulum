ba4387ad13d0e9214143ca1c009afdb8c8edc80b
Olivier Romain <olivierromain140@gmail.com>
2022-11-17 13:34:11 -0500

    Ajout de la fonction qui permet de retourner la date pour enregistrer les fichiers de données.

---
+++ Other_functions.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+def get_saving_name():
+    now = datetime.now()
+    path = str(now).replace("-", "_").replace(".", "_").replace(" ", "_").replace(":", "_")
+    return path[:-2]
