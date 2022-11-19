cd2350fd5b4dcba46273440554de85ddef3533c8
Olivier Romain <olivierromain140@gmail.com>
2022-11-18 19:35:43 -0500

    Ajout d'arguments lors de l'intitialisation d'un objet pendule pour mieux définir la fonction qui le drive. Ajout d'une méthode à la classe Pendulum qui retourne une fonction qui modélise la fonction qui drive le pendule. Ajout d'une méthode à la classe Pendulum qui retourne les paramètres du pendule sous la forme d'un dictionnaire. Ajout de lignes de code dans la méthode Pendulum.save_data() qui détectent si une simulation a déjà été faite avec les mêmes conditions initiales et qui enregistre les données du pendule si ce n'est pas le cas.

--- Pendule_class.py
+++ Pendule_class.py
@@ -1,16 +1,23 @@
 import numpy as np
 import Other_functions as of
 import pandas as pd
+import Parameters as param
 
 
 class Pendulum:
-    def __init__(self, teta, omega, t_range, driving_force, log_path):
+    def __init__(self, teta, omega, t_range, driving_force, driving_ang_freq, driving_delta, l, m, damping, g, log_path):
         """
         Initialisation des paramètres du pendule
         :param teta: Angle initial du pendule
         :param omega: Vitesse angulaire initiale du pendule
         :param t_range: Temps sur lesquels intégrer
-        :param driving_force: Fonction qui drive le pendule
+        :param driving_force: Grandeur de la force qui drive le pendule
+        :param driving_ang_freq: Fréquence (rad/s) de la force qui drive le pendule
+        :param driving_delta: Décalage (rad) de la force qui drive le pendule
+        :param l: Longueur du pendule
+        :param m: Masse du pendule
+        :param damping: Amortissement du pendule (multiple de g)
+        :param g: Accélération gravitationnelle
         :param log_path: Path du fichier où enregistrer les données à la fin d'une simulation
         """
         self.n_iter = len(t_range)  # Nombre total d'itération d'intégrations à effectuer
@@ -23,10 +30,29 @@ class Pendulum:
         self.omega = np.empty(self.n_iter)
 
         self.driving_force = driving_force
+        self.driving_ang_freq = driving_ang_freq
+        self.driving_delta = driving_delta
+        self.driving_function = self.get_driving_function()
+
+        self.l = l
+        self.m = m
+        self.g = g
+        self.damping = damping
+
         self.teta[0] = teta
         self.omega[0] = omega
 
         self.log_path = log_path
+
+    def get_driving_function(self):
+        """
+        Méthode qui retourne la fonction qui modélise la force qui drive le pendule.
+        :return: La fonction qui modélise la force qui drive le pendule.
+        """
+        def function(t):
+            return self.driving_force * np.cos(self.driving_ang_freq * t + self.driving_delta)
+
+        return function
 
     def evolute_one_step(self):
         """
@@ -58,8 +84,42 @@ class Pendulum:
         self.log_path .
         :return: None
         """
-        name = of.get_saving_name()
+        params = self.get_params()
+        # Ligne à ajouter dans le fichier de log pour garder une trace
+        param_stamp = str([params[p] for p in param.log_file_column_names])
+        time_stamp = of.get_saving_name()
+
+        with open(self.log_path, "r") as reader:
+            for line in reader.readlines():
+                if line.startswith(param_stamp):
+                    break
+
         col_names = ["t", "teta", "omega"]
         data = [self.temps, self.teta, self.omega]
 
         df = pd.DataFrame({cn: dt for cn, dt in zip(col_names, data)})
+        df.to_csv(time_stamp, sep=',', header=col_names, index=False)
+
+        log_string = param_stamp + time_stamp + "\n"
+        with open(self.log_path, "a") as writer:
+            writer.write(log_string)
+
+    def get_params(self):
+        """
+        Méthode qui retourne un dictionnaire contenant les paramètres du pendule.
+        :return: Dictionnaire qui contient les paramètres du pendule
+        """
+        dic = {
+            "teta0": self.teta[0],
+            "omega0": self.omega[0],
+            "dt": self.dt,
+            "driving_force": self.driving_force,
+            "driving_ang_freq": self.driving_ang_freq,
+            "driving_delta": self.driving_delta,
+            "l": self.l,
+            "m": self.m,
+            "damping": self.damping,
+            "g": self.g,
+        }
+
+        return dic
