b931c9af1ffc9cfee1829b59a4a2f40dda8d51ec
Olivier Romain <olivierromain140@gmail.com>
2022-11-18 19:36:56 -0500

    Ajout d'une liste dans le fichier Parameters.py qui définit les colonnes du fichier .csv qui enregistre les simulations qui ont été faites.

--- Parameters.py
+++ Parameters.py
@@ -2,3 +2,5 @@ from os import path
 
 
 data_path = "data"
+log_file_column_names = ["teta0", "omega0", "dt", "driving_force", "driving_ang_freq", "driving_delta",
+                         "l", "m", "damping", "g"]
