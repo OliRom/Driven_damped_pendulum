23232f6d157d027619c1d67bd80d5121176f6b49
Olivier Romain <olivierromain140@gmail.com>
2022-12-03 00:47:33 -0500

    Ajout de Animate_pendulum.py pour afficher l'annimation d'une simulation.

---
+++ Animate_pendulum.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import pandas as pd
+import os
+import numpy as np
+
+data_path = os.path.join("D:", "Big_data", "PHS2107_mec_sup_projet_final", "2022_12_02_23_50_10_719002.csv")
+
+data = pd.read_csv(data_path)
+x = data["t"]
+y = data["teta"]
+
+fig, ax = plt.subplots(figsize=(10, 10))
+mass, = ax.plot([], [], "ob")
+tige, = ax.plot([], [], "r")
+image_path = "photo_prof.png"
+
+image = OffsetImage(plt.imread(image_path, format="png"), zoom=.2)
+ab = AnnotationBbox(image, (0, 0), frameon=False)
+sapin = ax.add_artist(ab)
+
+plt.xlim(-1.5, 1.5)
+plt.ylim(-1.5, 1.5)
+ax.axis("equal")
+
+
+def animate(i):
+    teta = y[i]
+    x_now = -np.sin(teta)
+    y_now = -np.cos(teta)
+    mass.set_data(x_now, y_now)
+    tige.set_data([0, x_now], [0, y_now])
+    sapin.xybox = (x_now, y_now)
+    return mass, tige
+
+
+ani = anim.FuncAnimation(fig, animate, frames=1000, interval=10)
+plt.show()
